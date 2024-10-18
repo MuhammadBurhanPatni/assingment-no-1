f63fea75e0eb986bedc539281a078ed5879de493
MuhammadBurhanPatni <patniburhan6@gmail.com>
2024-10-18 18:59:17 +0500

    changes are made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home(){
   return (<div >
-    <h1 className="uppercase text-purple-600 text-pretty text-ellipsis font-extrabold text-3xl pt-2 pl-2">this is home page </h1>
+    <h1 className="uppercase text-black text-pretty text-ellipsis font-extrabold text-3xl pt-2 pl-2">this is home page </h1>
     <h2 className="capitalize text-2xl m-2 text-pink-400 font-serif">personal information</h2>
-   <div> <h3 className="uppercase text-xl font-serif  text-yellow-400 hover:bg-black ">name : burhan</h3></div>
+   <div> <h3 className="uppercase text-xl font-serif  text-yellow-600 hover:bg-black ">name : burhan</h3></div>
     <p  className="capitallize italic pl-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptates dolor, animi quos vitae est illum, delectus
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis qui odit, doloribus enim laborum tempora assumenda, dignissimos te
       mpore quam, architecto libero debitis sequi quia quod distinctio impedit 
